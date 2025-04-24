7623db0fd5c85d34b4731f216e5f77103382accb
Malin Nilsson <131678594+MalinNilssonFF@users.noreply.github.com>
2025-04-24 16:31:56 +0200

    Update restricted.js

--- restricted.js
+++ restricted.js
@@ -14,7 +14,8 @@ fetch("/.auth/me")
       `;
     } else {
       userInfoDiv.innerHTML = "<p>You are not logged in.</p>";
-    });
+    }
+  });
 
 
 fetch("https://cipappservice-ghayfnbmdxhtbhaw.westeurope-01.azurewebsites.net/weatherforecast")
